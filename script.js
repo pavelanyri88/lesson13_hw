7a183a31fffa11a0a0c3c9c4ce8c616e2b55776c
Antipov Pavel Y <PAntipov@beeline.kz>
2024-09-25 11:30:57 +0500

    initial commit, no API script

---
+++ script.js
@@ -0,0 +1,20 @@
+const apiKey = 'YOUR_API_KEY';
+const apiUrl = 'https://v6.exchangerate-api.com/v6/' + apiKey + '/latest/KZT';
+
+async function getExchangeRates() {
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('Ошибка при получении данных');
+        }
+        const data = await response.json();
+        showExchangeRates(data.conversion_rates);
+    } catch (error) {
+        document.getElementById('def_rate').textContent = 'Ошибка: ' + error.message;
+    }
+}
+
+function showExchangeRates(rates) {
+    const defRate = `1 KZT = ${rates.USD} USD, ${rates.EUR} EUR`;
+    document.getElementById('def_rate').textContent = defRate;
+}
