88783ad7a403a2001003e1631b5e97f04aad8d4c
Antipov Pavel Y <PAntipov@beeline.kz>
2024-09-25 15:33:49 +0500

    add converter for tests

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const apiKey = '9e03284219d5c6d252a17057';
 const apiUrl = 'https://v6.exchangerate-api.com/v6/'+apiKey+'latest/KZT';
 const defaultRate = document.getElementById('def_rate');
+const convertRateBtn = document.getElementById('convert');
+
 
 async function getExchangeRates() {
     try {
@@ -10,6 +12,7 @@ async function getExchangeRates() {
         }
         const data = await response.json();
         showExchangeRates(data.conversion_rates);
+        return data.conversion_rates;
     } catch (error) {
         defaultRate.textContent = 'Ошибка: ' + error.message;
     }
@@ -17,9 +20,37 @@ async function getExchangeRates() {
 
 function showExchangeRates(rates) {
     const defRate = `1 KZT = ${rates.USD} USD, ${rates.EUR} EUR`;
-    console.log(defRate);
     defaultRate.textContent = defRate;
 }
+
+function convert(input_cur, a_cur, b_cur, rates) {
+    if (a_cur === b_cur) return input_cur;
+
+    let convertedAmount;
+    if (a_cur=== 'KZT') {
+        convertedAmount = input_cur * rates[b_cur];
+    } else if (b_cur === 'KZT') {
+        convertedAmount = input_cur / rates[a_cur];
+    } else {
+        convertedAmount = (input_cur * rates[a_cur]) / rates[b_cur];
+    }
+    return convertedAmount.toFixed(2);
+}
+
+convertRateBtn.addEventListener('click', async() => {
+    var input_cur = parseFloat(document.getElementById('input_cur').value);
+    const a_cur = document.getElementById('a_cur').value;
+    const b_cur = document.getElementById('b_cur').value;
+
+    const rates = await getExchangeRates();
+    console.log(rates);
+    if (rates) {
+        const convert_result = convert(input_cur, a_cur, b_cur, rates);
+        console.log(convert_result);
+        document.getElementById('result').innerHTML = 'Результат '+input_cur+' '+a_cur+' равно '+convert_result+' '+b_cur;
+        document.getElementById('result').style.display="block";
+    }
+});
 
 getExchangeRates();
 
